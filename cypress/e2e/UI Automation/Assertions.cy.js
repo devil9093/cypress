fdd09c968a246722f0f5ee32becd44b3121d46bc
devil9093 <vkd9093@gmail.com>
2024-11-09 23:24:28 +0530

    radiobutton and checkbox

--- cypress/e2e/UI Automation/Assertions.cy.js
+++ cypress/e2e/UI Automation/Assertions.cy.js
@@ -1,5 +1,5 @@
 describe('assertions',() =>{
-    it.only('Implicit assertion',() =>{
+    it('Implicit assertion',() =>{
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
         //multiple assertion with should & and
         cy.url().should('include', 'orangehrmlive')
@@ -19,16 +19,28 @@ describe('assertions',() =>{
         cy.get("[type = 'submit']").should('be.enabled').click()
         //element should be exist on dom
         cy.get('.oxd-userdropdown-tab').should('exist')
-
-
-
-
     })
     it('Explicit assertions',() =>{
-        cy.visit('www.amazon.in');
-        cy.xpath('').type('shirt');
-        cy.xpath('').click().end();
-        //cy.get('nav-logo-sprites').contains('amazon').end();
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+        cy.title().should('include', 'Orange')
+        .and('eq', 'OrangeHRM')
+        .and('contain', 'Orange')
+        .and('not.contain', 'orangeHRM')
+        cy.get('.orangehrm-login-branding >img').should('be.visible')
+        cy.get("[name = 'username']").should('be.empty').type('Admin')
+        cy.get("[name = 'password']").should('be.empty').type('admin123')
+        cy.get("[type = 'submit']").should('be.enabled').click()
+        //element should be exist on dom
+        cy.get('.oxd-userdropdown-tab').should('exist')
+
+        //get the username and validate name need to change everyime
+        let expname = 'mandaJane user';
+        cy.get('.oxd-userdropdown-tab').then((x)=>{
+            let actname = x.text()
+            expect(actname).to.eq(expname)
+        })
+
     })
 
 })
---
+++ cypress/e2e/UI Automation/radiobuttonandcheckbox.cy.js
@@ -0,0 +1,27 @@
+describe('interaction with UI', ()=>{
+ it('radiobutton', () =>{
+    cy.visit("https://testautomationpractice.blogspot.com/")
+    cy.get('#male').should('be.visible')
+    //check is used to select radio button 
+    cy.get('#male').check().should('be.checked')
+    //assertion of correct radio button selected
+    cy.get('#male').should('be.checked')
+    cy.get('#female').should('not.be.checked')
+
+ })
+
+ it('radiobutton', () =>{
+    cy.visit("https://testautomationpractice.blogspot.com/")
+    cy.get('#sunday').should('be.visible')
+    //check and uncheck sunday check box
+    cy.get('#sunday').check().should('be.checked')
+    cy.get('#sunday').uncheck().should('not.be.checked')
+
+    //check and uncheck monday check box
+    cy.get('#monday').should('be.visible')
+    cy.get('#monday').check().should('be.checked')
+    cy.get('#monday').uncheck().should('not.be.checked')
+
+
+})
+})
